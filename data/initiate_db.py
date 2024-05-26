692969199e150dfc77d38bf69750c43b5f1711d0
FazreilieAlep <mrparksaereilee@gmail.com>
2024-05-26 12:59:24 +0800

    update backend

--- data/initiate_db.py
+++ data/initiate_db.py
@@ -1,15 +1,16 @@
 from data.models import Host, Web_API
 
-organization_name = "government of malaysia"
-domain_name = "api.data.gov.my/"
-path = "my_gov"
+host = Host(organization_name="government of malaysia", domain_name="api.data.gov.my/", path="my_gov", acronyms="dosm")
+host.save()
+
+host2 = Host(organization_name="bank negara malaysia", domain_name="api.bnm.gov.my/", path="bnm", acronyms="bnm")
+host2.save()
+
+
 url_list = [{"data": "malaysia economic indicator", "url": "data-catalogue?id=economic_indicators", "path": "economic-indicator"}, 
             {"data": "monetory aggregates", "url": "data-catalogue?id=monetary_aggregates", "path": "monetory-aggregates"},
             {"data": "household income by percentile", "url": "data-catalogue?id=hies_malaysia_percentile", "path": "household-income-by-percentile"},
             {"data": "household income and expenditure: states", "url": "data-catalogue?id=hies_state", "path": "household-income-and-expenditure-states"},]
-
-host = Host(organization_name=organization_name, domain_name=domain_name, path=path)
-host.save()
 
 for item in url_list:  # Iterate over each dictionary in url_list
     data = item["data"]  # Access the "data" key
