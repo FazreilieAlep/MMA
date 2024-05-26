1a36deed74c86ce66a1178701d1e119b79bdcd4e
Fazreilie Bin Alep <90892038+FazreilieAlep@users.noreply.github.com>
2024-05-26 15:26:44 +0800

    Update http.js

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://mma-five.vercel.app/',
   headers: {
     'Content-Type': 'application/json'
   }
