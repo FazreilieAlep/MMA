731f256402913ede70441778dbaf7fd79e482587
FazreilieAlep <mrparksaereilee@gmail.com>
2024-05-26 12:09:26 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mma.com.my/",
+  base: "/MMA/",
   plugins: [vue()],
   resolve: {
     alias: {
